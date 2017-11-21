9f267f246824293b63b23be3ebb94dbdd79ec1d3
jihwann <31734147+jihwann@users.noreply.github.com>
2017-11-21 18:20:29 +0900

    db1
    db1

---
+++ app/src/main/java/com/hansung/android/projectandroid2/DBHelper.java
@@ -0,0 +1,89 @@
+package com.hansung.android.projectandroid2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by jhim0 on 2017-11-17.
+ */
+
+public class DBHelper extends SQLiteOpenHelper{
+
+    final static String TAG= "GBHouse";
+
+    public DBHelper(Context context) {
+        super(context, UserContract.DB_NAME, null, UserContract.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i(TAG,getClass().getName()+".onCreate()");
+        db.execSQL(UserContract.Users.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        Log.i(TAG,getClass().getName() +".onUpgrade()");
+        db.execSQL(UserContract.Users.DELETE_TABLE);
+        onCreate(db);
+    }
+
+    //데이터베이스에 정보삽입
+    public long insertUserByMethod(String name, String address, String phone, String picture ) {     //picture 스트링 말고
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(UserContract.Users.KEY_NAME, name);
+        values.put(UserContract.Users.KEY_ADDRESS, address);
+        values.put(UserContract.Users.KEY_PHONE, phone);
+        values.put(UserContract.Users.KEY_PICTURE, picture);
+        return db.insert(UserContract.Users.TABLE_NAME,null,values);
+    }
+
+    public Cursor getAllUsersBySQL() {
+        String sql = "Select * FROM " + UserContract.Users.TABLE_NAME;
+        return getReadableDatabase().rawQuery(sql,null);
+    }
+
+
+
+
+
+    /*public Cursor getAllUsersBySQL_new() {
+        String sql = "Select Name FROM " + UserContract.Users.TABLE_NAME;
+        return getReadableDatabase().rawQuery(sql,null);
+    }*/
+
+/*
+    public Cursor getAllUsersByMethod() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.query(UserContract.Users.TABLE_NAME,null,null,null,null,null,null);
+    }*/
+//
+//    public long deleteUserByMethod(String _id) {
+//        SQLiteDatabase db = getWritableDatabase();
+//
+//        String whereClause = UserContract.Users._ID +" = ?";
+//        String[] whereArgs ={_id};
+//        return db.delete(UserContract.Users.TABLE_NAME, whereClause, whereArgs);
+//    }
+//
+//    public long updateUserByMethod(String _id, String name, String address, String phone, String picture) { //picture 스트링 말고
+//        SQLiteDatabase db = getWritableDatabase();
+//
+//        ContentValues values = new ContentValues();
+//        values.put(UserContract.Users.KEY_NAME, name);
+//        values.put(UserContract.Users.KEY_ADDRESS, address);
+//        values.put(UserContract.Users.KEY_PHONE, phone);
+//        values.put(UserContract.Users.KEY_PICTURE, picture);
+//
+//        String whereClause = UserContract.Users._ID +" = ?";
+//        String[] whereArgs ={_id};
+//
+//        return db.update(UserContract.Users.TABLE_NAME, values, whereClause, whereArgs);
+//    }
+
+}
---
+++ app/src/main/java/com/hansung/android/projectandroid2/UserContract.java
@@ -0,0 +1,39 @@
+package com.hansung.android.projectandroid2;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by jhim0 on 2017-11-17.
+ */
+
+public class UserContract {
+    public static final String DB_NAME="restaurant.db";
+    public static final int DATABASE_VERSION = 1;
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private UserContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class Users implements BaseColumns {
+        public static final String TABLE_NAME="Restaurant";
+        public static final String KEY_NAME = "Name";
+        public static final String KEY_ADDRESS = "ADDRESS";
+        public static final String KEY_PHONE = "PHONE";
+        public static final String KEY_PICTURE = "image"; //사진 스트링 말고...
+
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
+                " (" +
+                _ID + " INTEGER PRIMARY KEY" + COMMA_SEP +    //ID가 굳이 필요할까??
+                KEY_NAME + TEXT_TYPE + COMMA_SEP +
+                KEY_ADDRESS + TEXT_TYPE + COMMA_SEP +
+                KEY_PHONE + TEXT_TYPE + COMMA_SEP +
+                KEY_PICTURE + TEXT_TYPE +
+                ")";
+
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+
+}
